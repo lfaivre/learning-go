531d1440127eda215f023dbb4d3efbb57423b40d
Lorenzo Faivre <lfaivre@asu.edu>
2020-07-10 20:31:48 -0700

    feat: working with loops

--- cmd/learning-go/main.go
+++ cmd/learning-go/main.go
@@ -5,57 +5,41 @@ import (
 )
 
 func main() {
-	// basic switch statement
-	// "2" is referred to as a "tag"
-	// "break" keyword is implied
-	// can still use "break" however if needed
-	switch 5 {
-	case 1, 3, 5, 7, 9:
-		fmt.Println("Odd")
-	case 2, 4, 6, 8, 10:
-		fmt.Println("Even")
-		break
-		fmt.Println("Will not run")
-	default:
-		fmt.Println("Default")
+	// basic for-loop
+	for i := 0; i < 5; i++ {
+		// fmt.Println(i)
 	}
 
-	// switch statement with initializer
-	switch i := 2 + 3; i {
-	case 1, 3, 5, 7, 9:
-		fmt.Println("Odd")
-	case 2, 4, 6, 8, 10:
-		fmt.Println("Even")
-	default:
-		fmt.Println("Default")
+	// while loop (modified for-loop)
+	for i := 0; i < 5; {
+		// fmt.Println(i)
+		i++
 	}
 
-	// tagless syntax
-	// overlapping is allowed, first to equal true will evaluate
-	// use "fallthrough" to fall through to next case
-	j := 10
-	switch {
-	case j <= 10:
-		fmt.Println("First")
-	case j <= 20:
-		fmt.Println("Second")
-		fallthrough
-	case j >= 0:
-		fmt.Println("Greater than or equal to 0")
-	default:
-		fmt.Println("Default")
+	// while loop (modified for-loop)
+	j := 0
+	for {
+		if j == 4 {
+			break
+		}
+		j++
+		fmt.Println(j)
 	}
 
-	// type switch
-	var k interface{} = 1.0
-	switch k.(type) {
-	case int:
-		fmt.Println("i is an int")
-	case float64:
-		fmt.Println("i is a float64")
-	case string:
-		fmt.Println("i is a string")
-	default:
-		fmt.Println("i is another type")
+	// use labels to break out of multiple loops
+Loop:
+	for k := 1; k <= 3; k++ {
+		for l := 1; l <= 3; l++ {
+			fmt.Println(k * l)
+			if k*l >= 3 {
+				break Loop
+			}
+		}
+	}
+
+	// for-range loop
+	s := []int{1, 2, 3}
+	for key, value := range s {
+		fmt.Println(key, value)
 	}
 }
