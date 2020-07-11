f988ac8e9f024e9af4f50e77803c2d961f2f3a39
Lorenzo Faivre <lfaivre@asu.edu>
2020-07-10 19:03:42 -0700

    feat: working with structs

--- cmd/learning-go/main.go
+++ cmd/learning-go/main.go
@@ -2,48 +2,61 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
+// utilizes compositional relationships, "has a" vs inheritance "is a"
+type Animal struct {
+	Height int
+	Width  int
+	Weight float32
+}
+
+// utilizing compositional embeedding
+type Dog struct {
+	Animal
+	Name   string `required max:"100"`
+	Age    int
+	Breed  string
+	Family []string
+}
+
 func main() {
-	// map example
-	statePopulations := map[string]int{
-		"California": 39250017,
-		"Texas":      39250017,
-		"Florida":    39250017,
-		"New York":   39250017,
-		"Illinois":   39250017,
-		"Ohio":       39250017,
+	aDog := Dog{
+		Name:  "Barker",
+		Age:   1,
+		Breed: "Golden Retriever",
+		Family: []string{
+			"Buster",
+			"Frodo",
+		},
 	}
-	fmt.Println(statePopulations)
+	fmt.Println(aDog)
+	fmt.Println(aDog.Name)
+	fmt.Println(aDog.Family[1])
 
-	// map example using make
-	statePopulations2 := make(map[string]int)
-	fmt.Println(statePopulations2)
+	// utilizing composition
+	aDog.Height = 32
+	aDog.Width = 64
+	aDog.Weight = 100
+	fmt.Println(aDog)
 
-	// get length of map
-	fmt.Println(len(statePopulations))
+	// anonymous struct
+	aCat := struct{ name string }{name: "Whiskers"}
+	fmt.Println(aCat)
+	fmt.Println(aCat.name)
 
-	// access value of map element using key
-	fmt.Println(statePopulations["Ohio"])
+	// structs use values, not references
+	anotherCat := aCat
+	anotherCat.name = "Socks"
+	fmt.Println(aCat.name)
+	fmt.Println(anotherCat.name)
 
-	// add key/value to map
-	statePopulations["Georgia"] = 10310371
-	fmt.Println(statePopulations)
+	// working with tags
+	t := reflect.TypeOf(Dog{})
+	field, _ := t.FieldByName("Name")
+	fmt.Println(field.Tag)
 
-	// delete key/value from map
-	delete(statePopulations, "Georgia")
-	fmt.Println(statePopulations)
-
-	// ensure key/value exists using ok
-	pop, ok := statePopulations["Georgia"]
-	fmt.Println(pop, ok)
-
-	// maps pass value by reference
-	sp := statePopulations
-	fmt.Println(statePopulations)
-	fmt.Println(sp)
-	delete(sp, "Ohio")
-	fmt.Println(statePopulations)
-	fmt.Println(sp)
-
+	// tags are meaningless by themselves (just strings)
+	// needs a validation framework to handle tag values
 }
